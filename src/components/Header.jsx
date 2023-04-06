1b25068813a68c3d9a5360ed47329f3305072c87
Mauricio Cruz <mawcp7@gmail.com>
2023-04-06 11:15:29 -0600

    add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+        <div className="row">
+            <div className="blog-header">
+                <h1 className="blog-title text-primary">Generador de codigos QR</h1>
+                <p className="lead blog-description">Genera rápidamente un QR que redirija hacia el sitio que tú prefieras, página web o red social, ¡descargalo y compartelo!</p>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
